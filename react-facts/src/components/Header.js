0f06e8c62ad98a83719326f358b98db50e19f448
Harmeet Matharoo <hmjatt@gmail.com>
2022-08-21 19:57:09 -0600

    add `toggle mode ` button and logic for it

--- react-facts/src/components/Header.js
+++ react-facts/src/components/Header.js
@@ -1,13 +1,25 @@
 import logo from "../logo.svg";
 
-function Header() {
+function Header(props) {
     return (
         <header className="App-header">
-            <nav>
-                <img src={logo} className="App-logo" alt="logo" />
-                <h3 className="nav-logo-text">ReactFacts</h3>
-                <h4 className="nav-title">React Project</h4>
-            </nav>
+			<nav className={props.darkMode ? "dark": ""}>
+				<img src={logo} className="App-logo" alt="logo" />
+				<h3 className="nav-logo-text">ReactFacts</h3>
+				
+				<div 
+					className="toggler" 
+				>
+					<p className="toggler-light">Light</p>
+					<div 
+						className="toggler-slider"
+						onClick={props.toggleDarkMode}
+					>
+						<div className="toggler-slider-circle"></div>
+					</div>
+					<p className="toggler-dark">Dark</p>
+				</div>
+        	</nav>
         </header>
     );
 }
